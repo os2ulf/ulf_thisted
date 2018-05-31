d9584be2854e57e59a1ab9fb99456a830c2cb219
Martin Yde Granath <my@aarhus.dk>
2018-05-31 10:48:56 +0200

    SUPPORT-1591: Added siteimprove

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 #ULF changelog
 
+#2.5.1
+* SUPPORT-1591: Added siteimprove
+
 #2.5.0
 * First release, based on ULF v.2.5.0
 
---
+++ scripts/siteimprove.js
@@ -0,0 +1,5 @@
+(function() {
+  var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+  sz.src = '//ssl.siteimprove.com/js/siteanalyze_273508.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+})();
